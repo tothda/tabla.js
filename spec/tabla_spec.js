f719f5a25c9ecf4cdff05e931db5d2b015f2d9ce
Toth David <tothda@gmail.com>
2013-03-17 23:15:47 +0100

    More features and tests.

--- spec/tabla_spec.js
+++ spec/tabla_spec.js
@@ -1,10 +1,74 @@
 describe("Tabla", function(){
-  beforeEach(function(){
-    loadFixtures('example-1.html');
+  it("should be instantiated without arguments", function(){
+    expect(new Tabla()).not.toBe(null);
   });
 
-  it("should be instantiated without arguments", function(){
-    new Tabla();
-    expect(new Tabla()).not.toBe(null);
+  describe("With a when/then rule", function(){
+    var tabla, spy, fnResult;
+
+    beforeEach(function(){
+      var fn = function() { return fnResult; };
+      spy = jasmine.createSpy();
+      tabla = (new Tabla()).
+        withAction(spy).
+        whenMatches(fn).args(1,2,3).
+        build();
+    });
+
+    it("calls the action when when is true", function(){
+      fnResult = true;
+      tabla();
+      expect(spy).toHaveBeenCalledWith(1,2,3);
+    })
+
+    it("doesn't call the action when the rule doesn't match", function(){
+      fnResult = false;
+      tabla();
+      expect(spy).not.toHaveBeenCalled();
+    })
+
+  });
+});
+
+describe("Tabla.Term", function(){
+  it("Cannot be instantiated without argument.", function(){
+    expect(function(){ new Tabla.Term(); }).toThrow(new TypeError("Unexpected arguments."));
+  });
+
+  describe("jQuery element + value", function(){
+    var checkbox, term;
+
+    beforeEach(function(){
+      checkbox = $('<input type="checkbox" checked="checked"/>');
+      term = new Tabla.Term(checkbox, true);
+    });
+
+    it("Cannot be instantiated..", function(){
+      expect(new Tabla.Term(checkbox, true));
+    });
+
+    it("Checkbox: when checked then matches true", function(){
+      expect(term.evaluate()).toBeTruthy();
+    });
+
+    it("Checkbox: when not checked then matches false ", function(){
+      checkbox.trigger('click');
+      expect(term.evaluate()).toBeFalsy();
+    });
+  });
+
+  describe("With binary function", function(){
+    it("Evaluates to the same value as the fn.", function(){
+      var falseTerm = new Tabla.Term(function(){return false;});
+      var trueTerm = new Tabla.Term(function(){return true;});
+      expect(falseTerm.evaluate()).toBeFalsy();
+      expect(trueTerm.evaluate()).toBeTruthy();
+    });
+  });
+});
+
+describe("Tabla.Rule", function(){
+  it("Can be instantiated", function(){
+    expect(new Tabla.Rule()).not.toBe(null);
   });
 });
--- src/tabla.js
+++ src/tabla.js
@@ -1,84 +1,4 @@
 (function(context) {
-
-  var log = function() {
-    if (Tabla.LOGGING) {
-      var args = _.toArray(arguments);
-      console.log.apply(console, args);
-    }
-  }
-
-  // Supported parameters:
-  // (elem, fn)
-  // (elem, string)
-  var Term = function(){
-    var args = _.toArray(arguments);
-
-    function val(elem) {
-      if (elem.is(':checkbox')) { return elem.is(':checked'); }
-      if (elem.is(':radio')) { return elem.filter(':checked').val(); }
-      return elem.val();
-    }
-
-    var me = this;
-    this.elem = args[0];
-    if ($.isFunction(args[1])) {
-      this.predicate = args[1];
-      this.fn = function() {
-        return me.predicate(val(me.elem));
-      }
-    } else {
-      this.value = args[1];
-      this.fn = function() {
-        return val(me.elem) === me.value;
-      };
-    }
-  };
-
-  Term.prototype.evaluate = function(){ return this.fn.call(); };
-
-  Term.prototype.toString = function(){
-    return this.elem.selector + '===' + this.value;
-  };
-
-  var Rule = function(table, terms){
-    this.table = table;
-    this.terms = terms;
-  };
-
-  Rule.prototype.args = function(args) {
-    this.args = args;
-  };
-
-  Rule.prototype.evaluate = function() {
-    var ruleMatches = true;
-    _.each(this.terms, function(term, j) {
-      if (!ruleMatches) { return; }
-      var result = term.evaluate();
-        log('    ' + (j+1) + '. Term:', term.toString(), result);
-        if (!result) { ruleMatches = false; }
-    });
-    return ruleMatches;
-  };
-
-  Rule.prototype.runEnterAction = function() {
-    this.table.enterAction.apply(undefined, this.args);
-  };
-
-  Rule.prototype.runExitAction = function() {
-    this.table.exitAction.apply(undefined, this.args);
-  };
-
-  Rule.prototype.runAction = function() {
-    if (this.table.action) {
-      this.table.action.apply(undefined, this.args);
-    } else {
-      // In this case we assume the table has an enter and an exit action.
-      var exitingRule = this.table.matching;
-      if (exitingRule) { exitingRule.runExitAction(); }
-      this.runEnterAction();
-    }
-  };
-
   var Tabla = function(name) {
     this.rules = [];
     this.name = name;
@@ -167,6 +87,14 @@
     return this;
   };
 
+  TP.whenMatches = function() {
+    var terms = _.chain(arguments)
+                .map(function(fn){ return new Term(fn); })
+                .value();
+    this.rule = new Rule(this, terms);
+    return this;
+  };
+
   TP.when = function() {
     var args = _.toArray(arguments)
       , terms = _.chain(this.inputs)
@@ -202,5 +130,105 @@
 
   Tabla.any = function() { return true; }
 
+  var log = function() {
+    if (Tabla.LOGGING) {
+      var args = _.toArray(arguments);
+      console.log.apply(console, args);
+    }
+  }
+
+  // Supported parameters:
+  // (fn) - with binary function (returns true/false)
+  // (elem, fn) - with predicate function (binary fn with one parameter)
+  // (elem, string)
+  var Term = function(){
+    var args = _.toArray(arguments);
+
+    if (args.length === 0) {
+      throw new TypeError("Unexpected arguments.")
+    }
+
+    function val(elem) {
+      if (elem.is(':checkbox')) { return elem.is(':checked'); }
+      if (elem.is(':radio')) { return elem.filter(':checked').val(); }
+      return elem.val();
+    }
+
+    var me = this
+      , firstArg = args[0];
+
+    if (args.length === 1) {
+      if ($.isFunction(firstArg)) {
+        this.fn = firstArg;
+        return;
+      } else {
+        throw new TypeError("Argument error: should be a function.")
+      }
+    }
+
+    this.elem = firstArg;
+    if ($.isFunction(args[1])) {
+      this.predicate = args[1];
+      this.fn = function() {
+        return me.predicate(val(me.elem));
+      }
+    } else {
+      this.value = args[1];
+      this.fn = function() {
+        return val(me.elem) === me.value;
+      };
+    }
+  };
+
+
+  Term.prototype.evaluate = function(){ return this.fn.call(); };
+
+  Term.prototype.toString = function(){
+    if (this.elem) {return this.elem.selector + '===' + this.value;}
+    return "(fn)";
+  };
+
+  var Rule = function(table, terms){
+    this.table = table;
+    this.terms = terms;
+    this._args = [];
+  };
+
+  Rule.prototype.args = function(args) {
+    this._args = args;
+  };
+
+  Rule.prototype.evaluate = function() {
+    var ruleMatches = true;
+    _.each(this.terms, function(term, j) {
+      if (!ruleMatches) { return; }
+      var result = term.evaluate();
+        log('    ' + (j+1) + '. Term:', term.toString(), result);
+        if (!result) { ruleMatches = false; }
+    });
+    return ruleMatches;
+  };
+
+  Rule.prototype.runEnterAction = function() {
+    this.table.enterAction.apply(undefined, this._args);
+  };
+
+  Rule.prototype.runExitAction = function() {
+    this.table.exitAction.apply(undefined, this._args);
+  };
+
+  Rule.prototype.runAction = function() {
+    if (this.table.action) {
+      this.table.action.apply(undefined, this._args);
+    } else {
+      // In this case we assume the table has an enter and an exit action.
+      var exitingRule = this.table.matching;
+      if (exitingRule) { exitingRule.runExitAction(); }
+      this.runEnterAction();
+    }
+  };
+
+  Tabla.Term = Term;
+  Tabla.Rule = Rule;
   context.Tabla = Tabla;
 })(window);
